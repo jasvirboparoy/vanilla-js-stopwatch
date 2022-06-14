32a87a7d036c5f5e128750301459fb47b27779e8
Jasvir Boparoy <j.boparoy@student.unsw.edu.au>
2022-06-14 22:27:21 +1000

    V1 of vanilla-js stopwatch complete

---
+++ script.js
@@ -0,0 +1,74 @@
+let timerInterval;
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+let timerText = document.getElementById('timer');
+
+const getTimerText = () => {
+  let text = "";
+  if (hours < 10) {
+    text = text.concat("0", hours, ":");
+  } else {
+    text = text.concat(hours, ":");
+  }
+
+  if (minutes < 10) {
+    text = text.concat("0", minutes, ":");
+  } else {
+    text = text.concat(minutes, ":");
+  }
+
+  if (seconds < 10) {
+    text = text.concat("0", seconds);
+  } else {
+    text = text.concat(seconds);
+  }
+
+  return text;
+}
+
+const updateTimerUI = () => {
+  timerText.textContent = getTimerText();
+}
+
+let startBtn = document.getElementById('start-btn');
+startBtn.onclick = () => {
+  console.log('Start button clicked');
+
+  // Clear the timer interval first
+  clearInterval(timerInterval);
+
+  timerInterval = setInterval(() => {
+    seconds++;
+
+    if (seconds === 60) {
+      minutes++;
+      seconds = 0;
+
+      if (minutes === 60) {
+        hours++;
+        minutes = 0;
+      }
+    }
+
+    updateTimerUI();
+
+    console.log('hours: ', hours, ' minutes: ', minutes, ' seconds: ', seconds);
+  }, 1000);
+}
+
+let stopBtn = document.getElementById('stop-btn');
+stopBtn.onclick = () => {
+  console.log('Stop button clicked');
+  clearInterval(timerInterval);
+}
+
+let resetBtn = document.getElementById('reset-btn');
+resetBtn.onclick = () => {
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+  updateTimerUI();
+}
